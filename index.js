11121aa2723385ffa131df0a10a301f7ecf7cdf8
Pranevich, Aliaksandr <apranevi@visa.com>
2016-11-16 14:24:42 -0800

    no message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.sendfile('home.html', { root: __dirname + "/" } );
 })
 
 app.listen(app.get('port'), function() {
