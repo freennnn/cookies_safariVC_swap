db26f215f44c8831416e4d9093c06a6bb70e4030
Pranevich, Aliaksandr <apranevi@visa.com>
2016-11-22 12:39:53 -0800

    changed port to 5000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
-//app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000))
 //app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.json());
@@ -10,9 +10,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(3000, function() {
-  console.log("Listening on port %s...", server.address().port);
-});
+// var server = app.listen(3000, function() {
+//   console.log("Listening on port %s...", server.address().port);
+// });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
