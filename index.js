590c5b3d3124828f6408b629bf763fda561dd76e
Pranevich, Aliaksandr <apranevi@visa.com>
2016-12-01 14:32:05 -0800

    cookies 0.0.1 working version

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+//app.set('port', (process.env.PORT || 5000))
+
+var server = app.listen(3000, function() {
+  console.log("Listening on port %s...", server.address().port);
+});
+
 //app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 var routes = require("./routes/routes.js")(app);
-
-// var server = app.listen(3000, function() {
-//   console.log("Listening on port %s...", server.address().port);
-// });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
--- routes/routes.js
+++ routes/routes.js
@@ -4,15 +4,51 @@ var bodyParser = require('body-parser')
 
 var appRouter = function(app) {
   app.get('/', function(request, response) {
-    response.sendFile('home.html', { root: __dirname + "/" } );
+    response.sendFile('home.html', { root: __dirname + "/" } );Î
   })
 
   app.get("/ping", function(req, res) {
     res.send("ping ping")
   });
 
+  app.get('/link/urlscheme/:urlscheme/appname/:appname', function(req, res) {
+      if (req.params.urlscheme || req.params.appname) {
+          //Cookies.set('tratata', 'blablabla');
+          res.cookie(/*"Linked_" + */req.params.urlscheme, req.params.urlscheme + ';' +  req.params.appname);//.send('Added to cookies/linked');
+          console.log(res)
+          return res.sendFile('checkout_tracking.html', { root: __dirname + "/../" } );
+      } else {
+          return res.send(createError())
+      }
+  });
+
+  /*
+  app.get("/link/", function(req, res) {
+
+      // var urlScheme = req.params['urlscheme'];
+      // var appName = req.params['appname'];
+
+      var urlScheme = req.params[0];
+      var appName = req.params[1];
+
+      if (urlScheme && appName) {
+          response.sendFile('home.html', { root: __dirname + "/" } );
+      }
+      // if (req.param.urlscheme || req.param.appname) {
+      //       res.cookie("Linked_" + req.param.urlscheme, req.param.urlscheme + req.param.appname);
+      //       //return res.send(req.body);
+      //   } else {
+      //       return res.send(createError())
+      //   }
+    });
+
   app.post("/link", function(req, res) {
     //return res.send("link?")//+ req.body.urlsheme);
+    if (req.param.urlscheme || req.param.appname) {
+        res.cookie("Linked_" + req.param.urlscheme, req.param.urlscheme + req.param.appname);
+        return res.send(req.body);
+    }
+
     if (!req.body.urlscheme || !req.body.appname || !req.body.username) {
       console.log(req.body, req.body.urlscheme)
       return res.send(req.body);
@@ -27,9 +63,14 @@ var appRouter = function(app) {
         return res.send(req.body);
     }
   });
+*/
 
   app.get("/linked", function(req, res) {
-    return res.send(Cookies.get());
+      console.log(Cookies.get());
+      console.log("Cookies :  ", req.cookies);
+      res.sendFile('get_safari_cookies.html', { root: __dirname + "/../"} );
+  //    window.location = "payprovider1://req.cookies"
+  //    return res.send(req.cookies);
     //     console.log(req.cookies);
     //     console.log("linked API called")
     // res.send("blahblah")
